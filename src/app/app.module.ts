19566b760f9c534829eee1a54a5d9d7d3991558f
Pratimashendge <shendgepratima05@gmail.com>
2025-01-23 18:52:01 +0530

    done changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
+import { FormsModule } from '@angular/forms';
+import { PassengerModule } from './passenger/passenger.module';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FormsModule,
+    PassengerModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
